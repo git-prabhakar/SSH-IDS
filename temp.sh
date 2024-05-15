92da2872c72f9fc604330af4ccb40b49f79d2cf1
Prabhakar <97662264+git-prabhakar@users.noreply.github.com>
2024-05-15 19:21:57 -0400

    Update temp.sh

--- temp.sh
+++ temp.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
+
 current_epoch=$(date +%s)
-iptables -L INPUT -n --line-numbers | grep 'expire_at_' | while read line; do
-    rule_number=$(echo $line | cut -d' ' -f1)
-    expire_time=$(echo $line | grep -oP 'expire_at_\K\d+')
+
+iptables -L INPUT -n --line-numbers | grep 'expire_at_' | while read -r line; do
+    line_number=$(echo "$line" | awk '{print $1}')
+    ip_address=$(echo "$line" | awk '{print $4}')
+    expire_time=$(echo "$line" | grep -oP 'expire_at_\K\d+')
+
     if [ "$current_epoch" -gt "$expire_time" ]; then
-        iptables -D INPUT $rule_number
+        iptables -D INPUT $line_number
+        echo "Removed expired rule for IP $ip_address on line $line_number with expiration at $expire_time"
     fi
 done
